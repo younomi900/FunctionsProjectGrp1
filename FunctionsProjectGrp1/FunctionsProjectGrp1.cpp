ea15d8a60cf1120ceefbb714834089c39183e996
K00330070: Naomi Ambrose <K00330070@student.tus.ie>
2026-03-09 09:32:38 +0000

    Set up the main two functions that make up the project.

--- FunctionsProjectGrp1/FunctionsProjectGrp1.cpp
+++ FunctionsProjectGrp1/FunctionsProjectGrp1.cpp
@@ -19,6 +19,8 @@ int main()
     {
         cout << finalarray[i] << " ";
     }
+
+    ShowMenu();
 }
 
 void InputList(int array[], int size)
@@ -31,6 +33,10 @@ void InputList(int array[], int size)
 
 void ShowMenu()
 {
+    int optionnum;
 
+    cout << "\n 1. Display\n 2. GetTotal\n 3. GetAverage\n 4. GetLargest\n 5. GetSmallest\n 6. GetNumOcurrences\n 7. ScaleUp\n 8. Reverse\n 9. ZeroBase\n 10. RemoveNumber\n 11. Sort\n 12. Quit\n";
+
+    cin >> optionnum;
 }
 
